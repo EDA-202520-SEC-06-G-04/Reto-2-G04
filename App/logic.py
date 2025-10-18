5b68b212ced2eff6d053259ba1ddbb17efcdead3
JUANPABLO2007\penar <penarandajuan868@gmail.com>
2025-10-17 22:48:29 -0500

    view nuevo

--- App/logic.py
+++ App/logic.py
@@ -1,8 +1,10 @@
 import time
 import csv
 import sys
+import os
 from math import asin, sin, cos, sqrt, radians
 from datetime import datetime
+
 
 from DataStructures.Lists import array_list as al
 from DataStructures.Lists import single_linked_list as sl
@@ -10,6 +12,7 @@ from DataStructures.Queue import queue as q
 from DataStructures.Stack import stack as st
 from DataStructures.Maps import map_linear_probing as lp
 from DataStructures.Maps import map_separate_chaining as sc 
+
 
 data_structure = None
 
@@ -20,17 +23,17 @@ def new_logic(user_data_structure):
         data_structure = sl
 
     catalog = {"neighborhood":None,
-               "Taxis" : None
+               "taxis" : None
         } 
     catalog["neighborhood"] = data_structure.new_list()
-    catalog["Taxis"] = data_structure.new_list()
+    catalog["taxis"] = data_structure.new_list()
     return catalog
 
 def load_taxis_data(catalog, file_t):
     """
     Carga los datos del reto
     """
-    file_t = 'data_dir' + 'taxis-large.csv'
+    file_t = "Data/taxis-large.csv"
     input_del_archivo = csv.DictReader(open(file_t, encoding="utf-8"))
     for taxi in input_del_archivo:
         al.add_last(catalog['taxis'], taxi)
@@ -41,46 +44,17 @@ def load_neighborhoods_data(catalog, file_n):
     """
     Carga los datos del reto
     """
-    file_n =  'data_dir' + 'nyc-neighbothoods.csv'
+    file_n = "Data/taxis-large.csv"
     input_del_archivo = csv.DictReader(open(file_n, encoding="utf-8"))
     for neighborhood in input_del_archivo:
+        file_n = "Data/nyc-neighborhoods.csv"
         al.add_last(catalog['neighborhoods'], neighborhood)
-    size = al.size(catalog['neighbothoods'])
+        size = al.size(catalog['neighborhoods'])
     return size
 
-def req_1(catalog, fecha_y_hora_inicial, fecha_y_hora_final, tamanio_muestra):
+def req_1(catalog, ):
     
-    start_time = get_time()
-    
-    pickup_time = datetime.datetime.strptime(fecha_y_hora_inicial,"%Y - %m - %d %H:%M:%S")
-    dropoff_time = datetime.datetime.strptime(fecha_y_hora_final,"%Y - %m - %d %H:%M:%S")
-        
-    for viajes in catalog["taxis"]:
-        elementos_filtrados = 0
-        filtro = al.new_list()
-        pickup_datetime_taxi = catalog["taxis"]["pickup_datetime"][viajes]
-        dropoff_datetime_taxi = catalog["taxis"]["dropoff_datetime"][viajes]
-        
-        if pickup_datetime_taxi >= pickup_time and dropoff_datetime_taxi <= dropoff_time:
-            al.add_last(filtro, catalog["taxis"])
-            elementos_filtrados += 1
-        sort_crit = al.default_sort_criteria(pickup_datetime_taxi,pickup_time)
-        al.quick_sort(filtro, sort_crit)
-            
-        if elementos_filtrados <= (2 * tamanio_muestra):
-            rta = filtro
-                
-        else:
-            primero = al.sub_list(filtro,0,tamanio_muestra)
-            size = al.size(filtro)
-            ultimo = al.sub_list(filtro, size, tamanio_muestra) 
-            rta = primero,ultimo   
-        
-    end_time = get_time()      
-    tiempo = delta_time(start_time, end_time)            
-  
-    return elementos_filtrados,rta,tiempo
-
+    pass
 
 def req_2(catalog, lat_min, lat_max, N):
     """
@@ -111,114 +85,14 @@ def req_2(catalog, lat_min, lat_max, N):
     }
     
 
-
-def req_3(catalog):
-    """
-    Retorna el resultado del requerimiento 3
-    """
-    # TODO: Modificar el requerimiento 3
+def req_4(catalog):
+    
     pass
-
-
-def req_4(catalog, fecha_terminacion, momento, tiempo_referencia, tamanio_muestra):
-    
-    start_time = get_time()
-    elementos_filtrados = 0
-    limite = False
-    previo = sc.new_map(tamanio_muestra, 4)
-    posterior = sc.new_map(tamanio_muestra, 4)
-    filtro = al.new_list()
-    
-    fecha_ref = datetime.datetime.strptime(fecha_terminacion, "%Y-%m-%d").date()
-    hora_ref = datetime.datetime.strptime(tiempo_referencia, "%H:%M:%S").time()
-
-    for viajes in catalog["taxis"]:
-        dropoff_datetime_taxi = catalog["taxis"]["dropoff_datetime"][viajes]
-
-        if dropoff_datetime_taxi.date() == fecha_ref:
-            if momento == "DESPUES" and dropoff_datetime_taxi.time() > hora_ref:
-                al.add_last(filtro, catalog["taxis"][viajes])
-                elementos_filtrados += 1
-            elif momento == "ANTES" and dropoff_datetime_taxi.time() < hora_ref:
-                al.add_last(filtro, catalog["taxis"][viajes])
-                elementos_filtrados += 1
-    
-    al.selection_sort(filtro, "dropoff_datetime")
-
-    if elementos_filtrados <= (2 * tamanio_muestra):
-        rta = filtro
-    else:
-        primero = al.sub_list(filtro, 0, tamanio_muestra)
-        size = al.size(filtro)
-        ultimo = al.sub_list(filtro, size - tamanio_muestra, tamanio_muestra)
-
-        rta = al.new_list()
-        for i in range(al.size(primero)):
-            al.add_last(rta, al.get_element(primero, i))
-        for j in range(al.size(ultimo)):
-            al.add_last(rta, al.get_element(ultimo, j))
-
-
-    sc.put(previo, "cantidad_viajes", elementos_filtrados)
-    sc.put(posterior, "muestra", rta)
-
-    end_time = get_time()
-    tiempo = delta_time(start_time, end_time)
-    
-    return elementos_filtrados, rta, tiempo
 
 
 def req_5(catalog, datehour, N):
-    """
-    Retorna el resultado del requerimiento 5
-    """
-    # TODO: Modificar el requerimiento 5
-    import time
-    from datetime import datetime
-
-    start = time.process_time()
-
-    hash_table = catalog['hash_dropoff']  
-    trips = hash_table.get(datehour, [])  
-
-    trips.sort(key=lambda t: datetime.strptime(t['dropoff_datetime'], "%Y-%m-%d %H:%M:%S"), reverse=True)
-
-    total = len(trips)
-    end = time.process_time()
-    time_ms = (end - start) * 1000
-
-    if total <= 2 * N:
-        first, last = trips, []
-    else:
-        first, last = trips[:N], trips[-N:]
-
-    return {
-        'time_ms': round(time_ms, 2),
-        'total': total,
-        'first': first,
-        'last': last
-    }
-
-def req_6(catalog):
-    """
-    Retorna el resultado del requerimiento 6
-    """
-    # TODO: Modificar el requerimiento 6
+    
     pass
 
-
-# Funciones para medir tiempos de ejecucion
-
-def get_time():
-    """
-    devuelve el instante tiempo de procesamiento en milisegundos
-    """
-    return float(time.perf_counter()*1000)
-
-
-def delta_time(start, end):
-    """
-    devuelve la diferencia entre tiempos de procesamiento muestreados
-    """
-    elapsed = float(end - start)
-    return elapsed
+def req_6(catalog, datehour, N):
+    pass
--- App/view.py
+++ App/view.py
@@ -1,13 +1,22 @@
 import sys
-import tabulate
-
+from tabulate import tabulate
+from App import logic
+from DataStructures.Lists import array_list as al
+from DataStructures.Lists import single_linked_list as sl
+from App import logic as lg
 
 def new_logic():
     """
-        Se crea una instancia del controlador
+    Crea una instancia de la lógica según la estructura de datos seleccionada
     """
-    #TODO: Llamar la función de la lógica donde se crean las estructuras de datos
-    pass
+    print("Seleccione la estructura de datos que desea usar:")
+    print("1 - Array List")
+    print("2 - Single Linked List")
+    estructura = input("Ingrese el número de la estructura (1 o 2): ")
+
+    control = lg.new_logic(estructura)
+    return control
+
 
 def print_menu():
     print("Bienvenido")
@@ -25,7 +34,9 @@ def load_data(control):
     Carga los datos
     """
     #TODO: Realizar la carga de datos
-    pass
+    filename = "Data/taxis-test.csv"
+    catalog = lg.load_data(control, filename)
+    print("Taxis cargados:", al.size(catalog["array_list"]))
 
 
 def print_data(control, id):
@@ -40,11 +51,39 @@ def print_req_1(control):
         Función que imprime la solución del Requerimiento 1 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 1
-    pass
+    table=[]
+    start=input("Ingrese la fecha de inicio en formato  (%Y-%m-%d %H:%M:%S):")
+    end=input("Ingrese la fecha final en formato  (%Y-%m-%d %H:%M:%S):")
+    quantity=int(input("Ingrese la cantidad de trayectos a mostrar:"))
+    data=logic.req_1(control,start,end,quantity)
+    counter,answer,total_time,first,last=data
+    table.append(["Tiempo de carga en ms", total_time])
+    table.append(["Total de trayectos que cumplieron con el filtro", counter])
+    if answer==None:
+        for taxi in range(logic.al.size(first)):
+            t=logic.al.get_element(first, taxi)
+            table.append(formato_tabla("primeros trayectos", t))
+        for taxi in range(logic.al.size(last)):
+            t=logic.al.get_element(last, taxi)
+            table.append(formato_tabla("ultimos trayectos", t))
+    else:
+        for taxi in range(logic.al.size(answer)):
+            t=logic.al.get_element(answer, taxi)
+            table.append(formato_tabla("todos los trayectos trayectos", t))
+    print(tabulate(table,headers=["Datos", "Valores"], tablefmt="grid"))
+
+def formato_tabla(prefix, t):
+    inicio=t["start"]
+    lat_s= [round(t["pickup_lat"], 2), round(t["pickup_long"],2)]
+    fin=t["end"]
+    lat_e= [round(t["dropoff_lat"], 2), round(t["dropoff_long"],2)]
+    distancia= t["trip_distance"]
+    costo=t["total"]
+    return [f"{prefix}: {inicio}, Fin: {fin}, latitud_longitud_i: {lat_s}, latitud_longitud_f: {lat_e}, Distancia: {distancia}, Costo: {costo}"]
 
 
 def print_req_2(control):
-    """
+    """|
         Función que imprime la solución del Requerimiento 2 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 2
@@ -52,7 +91,7 @@ def print_req_2(control):
     lat_max = float(input("Ingrese la latitud máxima: "))
     N = int(input("Ingrese el número de trayectos a mostrar (N): "))
 
-    result = controller.req_2(control, lat_min, lat_max, N)
+    result = logic.req_2(control, lat_min, lat_max, N)
 
     print("\n=== Resultado Requerimiento 2 ===")
     print(f"Tiempo de ejecución: {result['time_ms']} ms")
@@ -64,8 +103,36 @@ def print_req_3(control):
         Función que imprime la solución del Requerimiento 3 en consola
     """
     # TODO: Imprimir el resultado del requerimiento 3
-    pass
+    min_distance=float(input("Ingrese la distancia minima (millas):"))
+    max_distance=float(input("Ingrese la distancia maxima (millas):"))
+    size_muestra=int(input("Ingrese el tamaño de la muestra:"))
 
+    resultado= logic.req_3(control, min_distance, max_distance, size_muestra)
+    print("Tiempo de ejecucion:", resultado[0], "ms")
+    print("Numero de trayectos en el rango", resultado[1])
+    headers=["Fecha-recogida", "Lat,long-recogida", "Fecha-terminacion", "Lat,Long-terminacion", "Distancia (mi)", "Costo(USD)"]
+ 
+    if resultado[1] >=size_muestra*2:
+        tabla_primeros= [[
+            trayecto["pickup_datetime"],
+            f"[{round(float(trayecto["pickup_latitude"]), 4)}, {round(float(trayecto["pickup_longitude"]),4)}]",
+            trayecto["dropoff_datetime"],
+            f"[{round(float(trayecto["dropoff_latitude"]), 4)}, {round(float(trayecto["dropoff_longitude"]),4)}]",
+            round(float(trayecto["trip_distance"]), 2),
+            round(float(trayecto["total_amount"]), 2)
+            ] for trayecto in resultado[2]["elements"]]
+        
+        tabla_ultimos= [[
+            trayecto["pickup_datetime"],
+            f"[{round(float(trayecto["pickup_latitude"]), 4)}, {round(float(trayecto["pickup_longitude"]),4)}]",
+            trayecto["dropoff_datetime"],
+            f"[{round(float(trayecto["dropoff_latitude"]), 4)}, {round(float(trayecto["dropoff_longitude"]),4)}]",
+            round(float(trayecto["trip_distance"]), 2),
+            round(float(trayecto["total_amount"]), 2)
+            ] for trayecto in resultado[3]["elements"]]
+    
+        print("\nPrimeros", size_muestra, "trayectos")
+        print(tabulate(tabla_primeros, headers=headers, tablefmt="fancy_grid"))
 
 def print_req_4(control):
     """
@@ -83,7 +150,7 @@ def print_req_5(control):
     datehour = input("Ingrese la fecha y hora (formato YYYY-MM-DD HH): ")
     N = int(input("Ingrese el número de trayectos a mostrar (N): "))
 
-    result = controller.req_5(control, datehour, N)
+    result = logic.req_5(control, lat_min, lat_max, N)
 
     print("\n=== Resultado Requerimiento 5 ===")
     print(f"Tiempo de ejecución: {result['time_ms']} ms")
@@ -128,7 +195,7 @@ def main():
         elif int(inputs) == 5:
             print_req_5(control)
 
-        elif int(inputs) == 5:
+        elif int(inputs) == 6:
             print_req_6(control)
 
         elif int(inputs) == 7:
